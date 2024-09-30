ba56b6cec7d8b602299f1fd47b330877aecb32b4
nomaanhusain <nomaanhusain@gmail.com>
2024-09-30 05:10:20 -0400

    report updated, more tests added

--- plots_and_scripts_argos/system_noise_plotter.py
+++ plots_and_scripts_argos/system_noise_plotter.py
@@ -5,16 +5,17 @@ import pandas as pd
 import numpy as np
 
 # File paths to all runs
-# files = ['m_system_noise_run1.json', 'm_system_noise_run2.json', 'm_system_noise_run3.json', 'm_system_noise_run4.json', 'm_system_noise_run5.json']
-files = ['m_low_system_noise_run1.json', 'm_low_system_noise_run2.json', 'm_low_system_noise_run3.json', 'm_low_system_noise_run4.json', 
-         'm_low_system_noise_run5.json']
+files = ['m_system_noise_run1.json', 'm_system_noise_run2.json', 'm_system_noise_run3.json', 'm_system_noise_run4.json', 'm_system_noise_run5.json']
+base_path="noise_json/"
+# files = ['m_low_system_noise_run1.json', 'm_low_system_noise_run2.json', 'm_low_system_noise_run3.json', 'm_low_system_noise_run4.json', 
+        #  'm_low_system_noise_run5.json']
 # files = ['system_noise_run5.json', 'system_noise_run9.json', 'system_noise_run10.json', 'system_noise_run11.json', 'system_noise_run12.json']
 # Initialize a list to store all noise data
 combined_noise_data = []
 
 # Iterate through all files and aggregate noise data
 for file in files:
-    with open(file) as f:
+    with open(base_path+file) as f:
         data = json.load(f)
         combined_noise_data.extend(data["noise_data"].values())
 
@@ -31,9 +32,10 @@ sns.boxplot(y='Noise', data=df, color='lightblue', width=0.3,showfliers=False)
 sns.stripplot(y='Noise', data=df, color='black', jitter=True, size=3)
 
 # Add titles and labels
-plt.title('Distribution of Personal Information Noise', fontsize=12)
-plt.ylabel('Sensor Noise Value', fontsize=12)
-
+plt.title('Distribution of Personal Information Noise', fontsize=16)
+plt.ylabel(r'Sensor Noise $\zeta$', fontsize=16)
+plt.yticks(fontsize=18)
+plt.ylim(0,1)
 # Show the plot
 plt.tight_layout()
 plt.show()
